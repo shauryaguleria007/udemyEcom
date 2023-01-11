8e4fe5b54170b1fd014e93dde6eab72c3c1d8420
shauryaguleria007 <shauryaguleria12345678@gmail.com>
2023-01-11 15:59:00 +0530

    buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <>
+      <Navbar />
+      <h1>sidebar</h1>
+      <Routes>
+        <Route path='/' element={<h1>home</h1>} />
+        <Route path='/about' element={<h1>about</h1>} />
+        <Route path='/cart' element={<h1>cart</h1>} />
+        <Route path='/products' element={<h1>products</h1>} />
+        <Route path='/products/:id' element={<h1>single product</h1>} />
+        <Route path='/checkout' element={<h1>checkout</h1>} />
+
+        <Route path='*' element={<h1>error</h1>} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
